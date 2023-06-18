3c00ad97f9ec2f375adb1178964cf267e1795454
Bernard Calma <nard.calma@gmail.com>
2023-06-18 11:07:22 -0700

    feat(Navbar): Add navbar component
    Imported style from github and add navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Navbar from './components/Navbar';
 
 const App = () => {
   return(
-    <div className='app'>
-      <h1>Hello World!!!</h1>
-    </div>
+    <main>
+      <Navbar />
+    </main>
   )
 }
 
