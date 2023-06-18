83a7b633bd73553ca1e851b9bf4d331e30d68ac7
Bernard Calma <nard.calma@gmail.com>
2023-06-18 11:19:05 -0700

    feat(cartItems): Import carItems inside cartSlice
    Change array to be cartItems

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,7 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
+import cartItems from "../../cartitems";
 
 const initialState = {
-    cartItems: [],
+    cartItems: cartItems,
     amount: 0,
     total: 0,
     isLoading: true
