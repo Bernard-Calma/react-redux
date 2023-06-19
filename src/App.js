1c30c8e58ee9d6cad484609c55b9fb153bd0fb21
Bernard Calma <nard.calma@gmail.com>
2023-06-19 02:12:36 -0700

    feat(Cart Items): Use api instead of local file for cart items
    Use asyncThunk to do api call in cartSlice reducer and create lifecycle

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import './App.css';
 import CartContainer from './components/CartContainer';
 import Navbar from './components/Navbar';
 import { useEffect } from 'react';
-import { calculateTotals } from './features/cart/cartSlice';
+import { calculateTotals, getCartItems } from './features/cart/cartSlice';
 import Modal from './components/Modal';
 
 
 const App = () => {
-  const {cartItems} = useSelector(store => store.cart)
+  const {cartItems, isLoading} = useSelector(store => store.cart)
   const {isOpen} = useSelector(store => store.modal)
   const dispatch = useDispatch();
 
@@ -16,13 +16,24 @@ const App = () => {
     dispatch(calculateTotals());
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [cartItems])
-  return(
-    <main>
-      {isOpen && <Modal />}
-      <Navbar />
-      <CartContainer />
-    </main>
-  )
+
+  useEffect(()=>{
+    dispatch(getCartItems())
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  },[])
+
+  if(isLoading)
+    return(<div className='loading'>
+      <h1>Loading...</h1>
+    </div>)
+  else
+    return(
+      <main>
+        {isOpen && <Modal />}
+        <Navbar />
+        <CartContainer />
+      </main>
+    )
 }
 
 export default App;
--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,9 +1,16 @@
-import { createSlice } from "@reduxjs/toolkit";
-import cartItems from "../../cartitems";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+const url ="https://course-api.com/react-useReducer-cart-project"
+
+export const getCartItems = createAsyncThunk("cart/getCartItems", () => {
+    return fetch(url)
+    .then(res => res.json())
+    .catch(err => console.log(err))
+})
 
 const initialState = {
-    cartItems: cartItems,
-    amount: 4,
+    cartItems: [],
+    amount: 0,
     total: 0,
     isLoading: true
 }
@@ -36,6 +43,20 @@ const cartSlice = createSlice({
             state.amount = amount;
             state.total = total;
         }
+    },
+    extraReducers: {
+        [getCartItems.pending]: state => {
+            state.isLoading = true;
+        },
+        [getCartItems.fulfilled]: (state, action) => {
+            state.isLoading = false;
+            // console.log(action);
+            state.cartItems = action.payload;
+        },
+        [getCartItems.rejected]: state => {
+            state.isLoading = false;
+        },
+        
     }
 })
 
