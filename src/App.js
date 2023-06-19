a9a8a00b7dd96fd8679a092e8390d09380f10987
Bernard Calma <nard.calma@gmail.com>
2023-06-19 01:12:00 -0700

    feat(CalculateTotal): Create calculateTotal reducer
    Add calucalteTotal functio inside cart reducers

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
 import CartContainer from './components/CartContainer';
 import Navbar from './components/Navbar';
+import { useEffect } from 'react';
+import { calculateTotals } from './features/cart/cartSlice';
+
 
 const App = () => {
+  const {cartItems} = useSelector(store => store.cart)
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(calculateTotals());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [cartItems])
   return(
     <main>
       <Navbar />
